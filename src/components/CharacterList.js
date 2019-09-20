817dd90d9e40fc29686db087746def428531ca43
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-20 12:49:11 +0100

    Remove unused vars

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from 'styled-components';
-import axios from 'axios';
 import CharacterCard from './CharacterCard';
 
 export default function CharacterList({characters, back, forward, setApiUrl}) {
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import styled from 'styled-components';
 
