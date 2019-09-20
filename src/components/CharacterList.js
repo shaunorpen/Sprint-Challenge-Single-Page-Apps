6275436bb0f151ffcda18d4e82e9d5efc0df23ed
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-20 08:57:14 +0100

    Add styling to CharacterList

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
 
 import characterApiResponse from '../data/characterApiRepsonse';
 
@@ -10,21 +11,48 @@ export default function CharacterList() {
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
 
+  const CharacterList = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+
+    div {
+      box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
+      padding: 1rem;
+      margin: 0.4rem 0.4rem;
+      flex-basis: 10rem;
+      flex-shrink: 1;
+      flex-grow: 1;
+      color: #472523;
+
+      h3 {
+        font-size: 1rem;
+        margin-top: 0;
+      }
+
+      p {
+        font-size: 0.8rem;
+        margin-bottom: 0;
+      }
+    }
+  `;
+
   return (
-    <section className="character-list">
+    <CharacterList>
       {
         characterApiResponse.results.map(character => {
+          const {id, name, species, gender, status, type} = character;
           return (
-            <div key={character.id} >
-              <h3>{character.name}</h3>
-              <p>Species: {character.species}</p>
-              <p>Gender: {character.gender}</p>
-              <p>Status: {character.status === 'unknown' ? 'Unknown' : character.status}</p>
-              <p>Type: {character.type ? character.type : 'Not defined'}</p>
+            <div key={id} >
+              <h3>{name}</h3>
+              <p>Species: {species}</p>
+              <p>Gender: {gender}</p>
+              <p>Status: {status === 'unknown' ? 'Unknown' : status}</p>
+              <p>Type: {type ? type : 'Not defined'}</p>
             </div>
           )
         })
       }
-    </section>
+    </CharacterList>
   );
 }
