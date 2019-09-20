5882faa4468ef71794bdc3ebe07046a9ac1f255c
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-20 15:10:42 +0100

    Add search box

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,13 +1,33 @@
 import React from "react";
 import styled from 'styled-components';
 import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 
 export default function CharacterList({characters, back, forward, setApiUrl}) {
 
   const CharacterList = styled.section`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+    .search {
+      display: flex;
+      justify-content: center;
+
+      input {
+        font-size: 1rem;
+        width: 50%;
+        border-radius: 10px;
+        border: none;
+        box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.3);
+        margin: 10px;
+        margin-bottom: 30px;
+        padding: 10px;
+        color: #472523;
+      }
+    }
+    
+    .character-cards {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+    }
 
     .nav {
       flex-basis: 100%;
@@ -34,11 +54,16 @@ export default function CharacterList({characters, back, forward, setApiUrl}) {
 
   return (
     <CharacterList>
-      {
-        characters.map(character => {
-          return <CharacterCard props={character} />
-        })
-      }
+      <div className='search'>
+        <SearchForm />
+      </div>
+      <div className='character-cards'>
+        {
+          characters.map(character => {
+            return <CharacterCard props={character} />
+          })
+        }
+      </div>
       <div className='nav'>
         <button onClick={() => setApiUrl(back)} >Back</button>
         <button onClick={() => setApiUrl(forward)} >Next</button>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function SearchForm() {
   return (
-    <div>
-      <input type='text' />
-    </div>
+    <input type='text' />
   );
 }
